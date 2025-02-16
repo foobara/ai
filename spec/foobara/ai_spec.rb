5825f5df4ecd8a38c900c346824e2a212e8a2485
Miles Georgi <azimux@gmail.com>
2025-02-16 10:36:59 -0800

    Create ruby project files

---
+++ spec/foobara/ai_spec.rb
@@ -0,0 +1,5 @@
+RSpec.describe Foobara::Ai do
+  it "has a version number" do
+    expect(Foobara::Ai::VERSION).to_not be_nil
+  end
+end
---
+++ version.rb
@@ -0,0 +1,5 @@
+module Foobara
+  module Ai
+    VERSION = "0.0.1".freeze
+  end
+end
